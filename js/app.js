c3629084a7d995e4a492e79231ec1ef8609256cd
victorieyo <thanhcong2004.1705@gmail.com>
2026-06-09 14:45:21 +1000

    added arena layout, symbol order shuffled

--- js/app.js
+++ js/app.js
@@ -1,6 +1,52 @@
 const startButton = document.getElementById("startBtn");
+const symbols = ["S", "D", "H", "C", "F"];
+const portals = document.querySelectorAll(".portal");
+let currentMode = "runner";
 
-startButton.addEventListener("click", () => {
-  document.getElementById("status").textContent = "Game Started!";
-  console.log("Game Started!");
+document.getElementById("runnerModeBtn").addEventListener("click", () => {
+  currentMode = "runner";
+  document.getElementById("status").textContent = "Runner Mode Selected";
 });
+
+document.getElementById("scModeBtn").addEventListener("click", () => {
+  currentMode = "sc";
+  document.getElementById("status").textContent = "Symbol Catcher Mode Selected";
+});
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  let sequence = [...symbols];
+  shuffle(sequence);
+
+  if (currentMode == "runner")
+}
+
+function shuffle(array) {
+  for (let i = 0; i < array.length; i++) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+}
+
+function clearPortals() {
+  portals.forEach(portal => {
+    portal.textContent = "";
+    portal.classList.add("hidden");
+  });
+}
+
+function showPortals() {
+  clearPortals();
+
+  let shuffledPortals = [...portals];
+  shuffle(shuffledPortals);
+
+  let activePortals = shuffledPortals.slice(0, 5);
+
+  for(let i = 0; i < 5; i++) {
+    activePortals[i].textContent = symbols[i];
+    activePortals[i].classList.remove("hidden");
+  }
+}
+
