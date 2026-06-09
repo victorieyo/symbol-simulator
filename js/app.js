3bb678de30414a4ac5333531afd8044182e4fd3c
victorieyo <thanhcong2004.1705@gmail.com>
2026-06-09 16:25:03 +1000

    runner mode

--- js/app.js
+++ js/app.js
@@ -1,7 +1,53 @@
 const startButton = document.getElementById("startBtn");
 const symbols = ["S", "D", "H", "C", "F"];
 const portals = document.querySelectorAll(".portal");
+const chatMessages = document.getElementById("chatMessages");
+const symbolNames1 = {
+  "S": ["S", "*"],
+  "D": ["D", "+"],
+  "H": ["H"],
+  "C": ["O", "C"],
+  "F": ["F", "Y"]
+};
+const symbolNames2 = {
+  "S": ["S", "Star", "*", "s", "star"],
+  "D": ["D", "Diamond", "+", "d", "diamond"],
+  "H": ["H", "Hourglass", "h", "hourglass"],
+  "C": ["O", "Circle", "C", "o", "c", "circle"],
+  "F": ["F", "Flower", "Y", "f", "flower", "y"]
+};
+const guildMembers = [
+  "Vertrauen",
+  "KouMizuki",
+  "Terravox",
+  "Eins",
+  "xThannatos",
+  "xDAsunaYuuki",
+  "fabrication",
+  "Eudazairon",
+  "IRubrub",
+  "Asalvo",
+  "Lycte",
+  "Wechsel",
+  "Charmer",
+  "Spaghettieis",
+  "MatchaPuff",
+  "Punchu",
+  "Chouquette",
+  "BasicColours",
+  "LabysZW",
+  "Hanso",
+  "Eirian",
+  "KimiSaru",
+  "MyeChu",
+  "evemorphs",
+  "HerculePoi",
+  "Zanika"
+];
+const order = ["st", "nd", "rd", "th", "th"];
 let currentMode = "runner";
+let currentRound = 0;
+let currentSequence = [];
 
 document.getElementById("runnerModeBtn").addEventListener("click", () => {
   currentMode = "runner";
@@ -13,13 +59,69 @@ document.getElementById("scModeBtn").addEventListener("click", () => {
   document.getElementById("status").textContent = "Symbol Catcher Mode Selected";
 });
 
+portals.forEach(portal => {
+  portal.addEventListener("click", () => {
+    if (portal.classList.contains("hidden")) {
+      return;
+    }
+    let clickedSymbol = portal.textContent;
+    if (clickedSymbol === currentSequence[currentRound]) {
+      currentRound++;
+
+      if (currentRound === 5) {
+        addChatMessage("You hit all 5 symbols!");
+        clearPortals();
+      }
+      else {
+        showPortals();
+      }
+    }
+    else {
+      addChatMessage(`Game: You flopped on the ${currentRound + 1}${order[currentRound]} round!`);
+      addChatMessage(`Game: You have killed your entire party and possibly their will to live. Good job!`);
+      clearPortals();
+    }
+  });
+});
+
 startButton.addEventListener("click", startGame);
 
 function startGame() {
+  chatMessages.innerHTML = "";
+  if (currentMode === "runner") {
+    startRunnerMode();
+  }
+  else {
+    startSCMode();
+  }
+}
+
+function startRunnerMode() {
+
+  currentRound = 0;
+  currentSequence = [...symbols];
+  shuffle(currentSequence);
+  let SCname = guildMembers[Math.floor(Math.random() * guildMembers.length)];
+
+  let curSeq = "";
+
+  for (const symbol of currentSequence) {
+    curSeq = curSeq + getRandomCallout(symbol) + " ";
+  }
+
+  addChatMessage(`${SCname}: ${curSeq}`);
+
+  showPortals();
+}
+
+function startSCMode() {
   let sequence = [...symbols];
   shuffle(sequence);
 
-  if (currentMode == "runner")
+  document.getElementById("status").textContent = "SC Mode Started";
+  document.getElementById("sequence").textContent = sequence.join(" ");
+
+  clearPortals();
 }
 
 function shuffle(array) {
@@ -50,3 +152,16 @@ function showPortals() {
   }
 }
 
+function getRandomCallout(symbol) {
+  const names = symbolNames1[symbol];
+  return names[Math.floor(Math.random() * names.length)];
+}
+
+function addChatMessage(message) {
+  const p = document.createElement("p");
+  p.classList.add("chat-message");
+  p.textContent = message;
+
+  chatMessages.appendChild(p);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+}
